4d33eb4f74ec258836283a4ba7e81fab0fdee2db
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-31 18:29:58 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,6 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
