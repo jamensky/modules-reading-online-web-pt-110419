f10b7676d2f79ea71dd23c158d0bbbf5029484ae
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-31 17:42:36 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,10 +4,10 @@ require_relative './class_methods_module.rb'
 class Kid
   include Dance
 
-  attr_accessor :name 
+  attr_accessor :name
 
   def initialize(name)
-    @name = name 
-  end 
+    @name = name
+  end
 
-end 
+end
